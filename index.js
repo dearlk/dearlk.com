f37ddacd7add7db31869578da1ce5579cc7852aa
Lalit Kumar <lalitksharma.com>
2018-04-21 21:50:46 +0800

    added login module

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var path = require('path');
 var fs = require('fs')
 var morgan = require('morgan')
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 
 
@@ -12,32 +15,61 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
+
 // Configuring the database
 var dbConfig = require('./config/database.config.js');
-var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url);
 mongoose.connection.once('open', function() {
     console.log("Successfully connected to the database");
 })
+var db = mongoose.connection;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'dearlk-web.log'), {flags: 'a'});
 // setup the logger
-app.use(morgan('full', {stream: accessLogStream}));
+app.use(morgan('combined', {stream: accessLogStream}));
 
 // Require Main routes
-require('./app/routes/main.routes.js')(app);
+var routes = require('./app/routes/router.js');
+app.use ('/',routes);
 
-// define a simple route
-app.get('/', function(req, res){
-    //res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-   res.render('index', {res});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+
 
 
 // listen for requests
